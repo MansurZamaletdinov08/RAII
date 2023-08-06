09e6721416dce84a29e41f2740846bc7058d3fa8
Mansur Zamaletdinov <zamaletdinov98@list.ru>
2023-08-06 22:11:20 +0300

    RAIIcommit

---
+++ SmartArray/SmartArray/SmartArray.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+
+
+class smart_array : public std::exception {
+	private:
+		int* arr = 0;
+		int count = 0;
+		int size = 0;
+public:
+	smart_array(const int size1) {
+		size = size1;
+
+		arr = new int[size];
+
+		for (int i = 0; i < size; i++) {
+			arr[i] = i;
+		}
+	}
+
+
+	void add_element(int val) {
+		if (count > size) {
+			throw std::runtime_error("the number of elements is greater than the number of elements for which memory is allocated.");
+		} 
+		else {
+			arr[count] = val;
+			++count;
+		}
+	}
+
+
+	int get_element(int index) {
+		if ((index >= size) || (index < 0)) {
+			throw exception("Error getting an element by index! index out of range");
+		}
+		else {
+			return arr[index];
+		}
+	}
+
+
+	~smart_array() {
+		delete[] arr;
+	}
+};
+
+
+int main()
+{
+	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		arr.add_element(14);
+		arr.add_element(15);
+
+		std::cout << arr.get_element(1) << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	return 0;
+}
